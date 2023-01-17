1d6ba1f1c544ecd4fa69884d4daa381e88f41053
luiss <luishuamanc.21@gmail.com>
2023-01-17 05:38:13 -0500

    commit prueba2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'esto-es-un-stringggg';
+const nuevaString = 'esto-es-un-string2';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 8;
